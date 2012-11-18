9163dc0fb1b8f2096325c4a89af0dfcf4fcc314e
shunirr <shunirr@gmail.com>
2012-11-19 00:47:52 +0900

    便利そうな BaseActivity 導入

--- src/jp/s5r/allegro/activities/BaseActivity.java
+++ src/jp/s5r/allegro/activities/BaseActivity.java
@@ -1,6 +1,49 @@
 package jp.s5r.allegro.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+import jp.s5r.allegro.utils.Log;
 
 public class BaseActivity extends Activity {
+  protected void onCreate(Bundle savedInstanceState) {
+    Log.i(getClass(),"#onCreate(Bundle)");
+    super.onCreate(savedInstanceState);
+  }
+
+  protected void onStart() {
+    Log.i(getClass(), "#onStart()");
+    super.onStart();
+  }
+
+  protected void onRestart() {
+    Log.i(getClass(), "#onRestart()");
+    super.onRestart();
+  }
+
+  protected void onResume() {
+    Log.i(getClass(), "#onResume()");
+    super.onResume();
+  }
+
+  protected void onPause() {
+    Log.i(getClass(), "#onPause()");
+    super.onPause();
+  }
+
+  protected void onStop() {
+    Log.i(getClass(), "#onStop()");
+    super.onStop();
+  }
+
+  protected void onDestroy() {
+    Log.i(getClass(), "#onDestroy()");
+    super.onDestroy();
+  }
+
+  @Override
+  public void finish() {
+    if (!isFinishing()) {
+      super.finish();
+    }
+  }
 }
